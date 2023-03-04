53eebf09729dcf6abd42079cbad2502960141849
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2023-03-04 23:22:29 +0530

    Updated Themes

--- react-guide-tutorial/src/Card.js
+++ react-guide-tutorial/src/Card.js
@@ -9,8 +9,8 @@ const Card = props => {
             <div className="container">
                 <h4><b>{props.name}</b></h4>
                 <p>{props.title}</p>
-                <input type = "text" onChange={props.onChangeInput} value = {props.name}/>
-                <p><button className="button" onClick={props.onChangeName}>Change Name</button></p>
+                <input type="text" onChange={props.onChangeName} value={props.name} />
+                <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
                 <div>{props.children}</div>
             </div>
         </div>
